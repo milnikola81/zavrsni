a114e4a340e7a5082eb3228d6e2ef6da7c542910
Nikola Milojevic <milnikola81@gmail.com>
2018-06-04 21:54:49 +0200

    zapoceo deseti zadatak. link prema single post stranici

--- posts.php
+++ posts.php
@@ -59,7 +59,7 @@
                 foreach ($posts as $post) {
             ?>
                 <div class="blog-post">
-                    <h2 class="blog-post-title"><a href="#"><?php echo($post['title']) ?></a></h2>
+                    <h2 class="blog-post-title"><a href="<?php echo($post['id']) ?>"><?php echo($post['title']) ?></a></h2>
                     <p class="blog-post-meta"><?php echo ($post['created_at']) ?> by <a href="#"><?php echo($post['author']) ?></a></p>
                     <p><?php echo ($post['body']) ?></p>
                 </div>
