1c2599f9c5e82016367fe9a628cd90c93da9ee86
Nikola Milojevic <milnikola81@gmail.com>
2018-06-05 19:34:19 +0200

    zavrsen zadatak 12

--- partials/dbconnection.php
+++ partials/dbconnection.php
@@ -3,7 +3,7 @@
     // obavezno ih ovde zamenite
     $servername = "127.0.0.1";
     $username = "root";
-    $password = "";
+    $password = "vivify";
     $dbname = "blog";
 
     try {
--- single-post.php
+++ single-post.php
@@ -18,23 +18,28 @@
             $id = intval($_GET['id']);
 
             // pripremamo upit
-            $sql = "SELECT id, title, body, author, created_at FROM posts WHERE id=$id";
-            $statement = $connection->prepare($sql);
+            $sqlPost = "SELECT * FROM posts WHERE id=$id";
+            $sqlComment = "SELECT * FROM comments WHERE post_id=$id";
+            $statementPost = $connection->prepare($sqlPost);
+            $statementComment = $connection->prepare($sqlComment);
+
 
             // izvrsavamo upit
-            $statement->execute();
+            $statementPost->execute();
+            $statementComment->execute();
 
             // zelimo da se rezultat vrati kao asocijativni niz.
             // ukoliko izostavimo ovu liniju, vratice nam se obican, numerisan niz
-            //$statement->setFetchMode(PDO::FETCH_ASSOC);
+            $statementComment->setFetchMode(PDO::FETCH_ASSOC);
 
             // punimo promenjivu sa rezultatom upita
-            $post = $statement->fetch();
+            $post = $statementPost->fetch();
+            $comments = $statementComment->fetchAll();
 
             // koristite var_dump kada god treba da proverite sadrzaj neke promenjive
                 /*
                 echo '<pre>';
-                var_dump($post);
+                var_dump($comments);
                 echo '</pre>';
                 */
 
@@ -48,6 +53,20 @@
                 <p><?php echo($post['body'])?></p>
 
             </div><!-- /.blog-post -->
+
+            <?php
+                foreach ($comments as $comment) {
+            ?>
+                <ul class="comment" style='color: #969696; list-style-type: none'>
+                    <li style='margin-bottom: 1rem'><strong><em><?php echo($comment['author']) ?></em></strong></li>
+                    <li><em><?php echo($comment['text']) ?></em></li>
+                    <hr>
+                </ul>
+
+            <?php
+                }
+            ?>
+
         </div>
 
         <?php
